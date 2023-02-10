456ed5cd74a3b1fa127f4fa518742d40d94863dc
Almas <90383195+Almas0007@users.noreply.github.com>
2023-02-10 21:24:53 +0600

    -first commit

---
+++ app/src/main/java/kz/mobydev/numbersystem/MainActivity.kt
@@ -0,0 +1,31 @@
+package kz.mobydev.numbersystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kz.mobydev.numbersystem.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding :ActivityMainBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.apply {
+            button.setOnClickListener{
+
+                result.text = convertNumber(input.text.toString(),fromBase.text.toString().toInt(),toBase.text.toString().toInt())
+            }
+
+
+        }
+
+    }
+
+    fun convertNumber(input: String, fromBase: Int, toBase: Int): String {
+        val decimalValue = java.lang.Long.parseLong(input, fromBase)
+        return java.lang.Long.toString(decimalValue, toBase).toUpperCase()
+    }
+
+
+}
