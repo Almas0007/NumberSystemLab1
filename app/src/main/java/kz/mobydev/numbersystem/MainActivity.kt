5fc9169c223a34e70705f15fe51187f89ee89830
Almas <90383195+Almas0007@users.noreply.github.com>
2023-02-10 23:52:34 +0600

    - final without validation

--- app/src/main/java/kz/mobydev/numbersystem/MainActivity.kt
+++ app/src/main/java/kz/mobydev/numbersystem/MainActivity.kt
@@ -6,15 +6,32 @@ import kz.mobydev.numbersystem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var binding : ActivityMainBinding? = null
+    private var fromBase:Int = 2
+    private var toBase:Int =2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
+
         binding?.apply {
+            btnBinInput.setOnClickListener { fromBase = 2 }
+            btnDecInput.setOnClickListener { fromBase = 10 }
+            btnOctInput.setOnClickListener { fromBase = 8 }
+            btnHexInput.setOnClickListener { fromBase = 16 }
+        }
+        binding?.apply {
+            btnBinOutput.setOnClickListener { toBase = 2 }
+            btnDecOutput.setOnClickListener { toBase = 10 }
+            btnOctOutput.setOnClickListener { toBase = 8 }
+            btnHexOutput.setOnClickListener { toBase = 16 }
+        }
 
-
-
+        binding?.apply {
+            btnConvert.setOnClickListener {
+                var input = editTextInput.text.toString()
+                textTvResult.text = convertNumber(input,fromBase, toBase).toString()
+            }
         }
 
     }
